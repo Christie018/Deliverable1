f1ca6e97ede82d62dc8bafe5b3a3f0b743779f8f
Christie018 <christie.sullivan018@gmail.com>
2023-12-22 22:54:13 -0500

    Creating restocking tool

--- Deliverable1/Program.cs
+++ Deliverable1/Program.cs
@@ -1,2 +1,86 @@
 ﻿// See https://aka.ms/new-console-template for more information
+int sodaRestock = 40;
+int sodaStock = 100;
+int chipRestock = 20;
+int chipStock = 40;
+int candyRestock = 40;
+int candyStock = 60;
+int input = 0;
+
 Console.WriteLine("Welcome to the restocking tool.");
+
+// Checking soda stock 
+Console.WriteLine("How many sodas have been sold today? 100 are in stock.");
+
+input = int.Parse(Console.ReadLine());
+
+if (input <= sodaStock) {
+    sodaStock -= input;
+    Console.WriteLine("There are " + sodaStock + " sodas left.");
+}
+else
+{
+    Console.WriteLine("That value is too high. Stock not adjusted.");
+}
+
+//Checking chip stock
+Console.WriteLine("How many chips have been sold today? 40 are in stock.");
+
+input = int.Parse(Console.ReadLine());
+
+if (input <= chipStock)
+{
+    chipStock -= input;
+    Console.WriteLine("There are " + chipStock + " sodas left.");
+}
+else
+{
+    Console.WriteLine("That value is too high. Stock not adjusted.");
+}
+
+//Checking candy stock
+Console.WriteLine("How much candy has been sold today? 60 are in stock.");
+
+input = int.Parse(Console.ReadLine());
+
+if (input <= candyStock)
+{
+    candyStock -= input;
+    Console.WriteLine("There are " + candyStock + " sodas left.");
+}
+else
+{
+    Console.WriteLine("That value is too high. Stock not adjusted.");
+}
+
+Console.WriteLine("Thank you for filling out the values. Here's what needs to be restocked:");
+
+//Checking soda restock
+if(sodaStock <= sodaRestock)
+{
+    Console.WriteLine("Soda needs to be restocked.");
+} else {
+    ;
+}
+
+//Checking chip restock
+if (chipStock <= chipRestock)
+{
+    Console.WriteLine("Chips need to be restocked.");
+}
+else
+{
+    ;
+}
+
+//Checking candy restock
+if (candyStock <= candyRestock)
+{
+    Console.WriteLine("Candy needs to be restocked.");
+}
+else
+{
+    ;
+}
+
+Console.WriteLine("Goodbye!");
--- Deliverable1/obj/Debug/net8.0/Deliverable1.AssemblyInfo.cs
+++ Deliverable1/obj/Debug/net8.0/Deliverable1.AssemblyInfo.cs
@@ -14,7 +14,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("Deliverable1")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+43cfbd78eb957b95241e7f0974ba2dd55e1be5fe")]
 [assembly: System.Reflection.AssemblyProductAttribute("Deliverable1")]
 [assembly: System.Reflection.AssemblyTitleAttribute("Deliverable1")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
